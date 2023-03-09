1e7e3783f6b3d1c6d6527616e8f0933e2fc3dfa6
Nuo Mi <nuomi2021@gmail.com>
2023-03-09 21:32:06 +0800

    alf: refact, expose classify as dsp function

--- libavcodec/vvcdsp.h
+++ libavcodec/vvcdsp.h
@@ -189,8 +189,11 @@ typedef struct VVCALFDSPContext {
     void (*filter_cc)(uint8_t *dst, ptrdiff_t dst_stride, const uint8_t *luma, ptrdiff_t luma_stride,
         int width, int height, int hs, int vs, const int8_t *filter, int vb_pos);
 
-    void (*get_coeff_and_clip)(int8_t *coeff, int16_t *clip,
-        const uint8_t *src, ptrdiff_t src_stride, int width, int height, int vb_pos,
+    void (*classify)(int *class_idx, int *transpose_idx,
+        const uint8_t *src, ptrdiff_t src_stride, int width, int height, int vb_pos);
+
+    void (*recon_coeff_and_clip)(int8_t *coeff, int16_t *clip,
+        const int *class_idx, const int *transpose_idx, int size,
         const int8_t *coeff_set, const uint8_t *clip_idx_set, const uint8_t *class_to_filt);
 } VVCALFDSPContext;
 
