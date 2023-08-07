3b00538825e852d5ea9d90b12c914f46433fa8d5
Frank Plowman <post@frankplowman.com>
2023-08-07 12:46:34 +0100

    tests/checkasm: Add VVC ITX test
    tests/checkasm/vvc_itx: Fix dst stride

--- tests/checkasm/vvc_itx.c
+++ tests/checkasm/vvc_itx.c
@@ -28,51 +28,79 @@
 #include "checkasm.h"
 
 #define SIZEOF_PIXEL ((bit_depth + 7) / 8)
-#define PIXEL_STRIDE (2*MAX_PB_SIZE + AV_INPUT_BUFFER_PADDING_SIZE)
-#define BUF_SIZE (PIXEL_STRIDE * MAX_TB_SIZE)
+#define BUF_SIZE (MAX_TB_SIZE * MAX_TB_SIZE)
 
-#define randomize_buffers(buf0, buf1, size)                                             \
-    do {                                                                                \
-        int k;                                                                          \
-        for (k = 0; k < size; ++k) {                                                    \
-            uint32_t r = rnd();                                                         \
-            int32_t a = INT16_MIN + r / (UINT32_MAX / (INT16_MAX - INT16_MIN + 1) + 1); \
-            AV_WN32A(buf0 + k, a);                                                      \
-            AV_WN32A(buf1 + k, a);                                                      \
-        }                                                                               \
+#define randomize_buffers(buf0, buf1, size, min, max)                           \
+    do {                                                                        \
+        int k;                                                                  \
+        for (k = 0; k < size; ++k) {                                            \
+            uint32_t r = rnd();                                                 \
+            int32_t a = min + r / (max / (max - min + 1) + 1);                  \
+            AV_WN32A(buf0 + k, a);                                              \
+            AV_WN32A(buf1 + k, a);                                              \
+        }                                                                       \
     } while (0)
 
-static void check_idct2(VVCDSPContext h, int bit_depth)
+const char *itx_str[N_TX_TYPE] = {
+    "dct2", // DCT2
+    "dst7", // DST7
+    "dct8", // DCT8
+};
+
+const int itx_log2_min_size[N_TX_TYPE] = {
+    1,  // DCT2
+    2,  // DST7
+    2,  // DCT8
+};
+
+const int itx_log2_max_size[N_TX_TYPE] = {
+    6,  // DCT2
+    5,  // DST7
+    5,  // DCT8
+};
+
+static void check_itx(VVCDSPContext h, enum TxType trh, enum TxType trv, int bit_depth)
 {
+    const int log2_transform_range = 15;
     LOCAL_ALIGNED_32(int, ref_dst, [BUF_SIZE]);
     LOCAL_ALIGNED_32(int, new_dst, [BUF_SIZE]);
     LOCAL_ALIGNED_32(int, ref_src, [BUF_SIZE]);
     LOCAL_ALIGNED_32(int, new_src, [BUF_SIZE]);
 
-    const ptrdiff_t stride = PIXEL_STRIDE * SIZEOF_PIXEL;
+    for (int log2_width = itx_log2_min_size[trh]; log2_width <= itx_log2_max_size[trh]; ++log2_width) {
+        for (int log2_height = itx_log2_min_size[trv]; log2_height <= itx_log2_max_size[trv]; ++log2_height) {
+            const int width = 1 << log2_width;
+            const int height = 1 << log2_height;
 
-    for (int log2_size = 1; log2_size <= 6; log2_size++) {
-        const int size = 1 << log2_size;
-        declare_func_emms(AV_CPU_FLAG_MMX, void, int *dst, ptrdiff_t dst_stride,
-                                                 int *src, ptrdiff_t src_stride);
+            declare_func_emms(AV_CPU_FLAG_MMX, void, int *dst, const int *src, int nzw, int log2_transform_range);
 
-        randomize_buffers(ref_src, new_src, BUF_SIZE);
-        memset(ref_dst, 0, BUF_SIZE);
-        memset(new_dst, 0, BUF_SIZE);
+            randomize_buffers(ref_src, new_src, BUF_SIZE, -(1 << (bit_depth - 1)), 1 << (bit_depth - 1) - 1);
+            memset(ref_dst, 0, BUF_SIZE);
+            memset(new_dst, 0, BUF_SIZE);
 
-        if (check_func(h.itx.itx[DCT2][log2_size - 1], "vvc_inv_dct2_%d", size)) {
-            call_ref(ref_dst, stride, ref_src, stride);
-            call_new(new_dst, stride, new_src, stride);
-            checkasm_check_int32_t("vvc_itx.asm", 0, ref_dst, stride * sizeof(int), new_dst, stride * sizeof(int), 1, size, "dst");
+            // @TODO: test extended precision (l2tr != 15)
+            // @TODO: test nzw != width
+            if (check_func(h.itx.itx[trh][trv][log2_width][log2_height],
+                           "inv_%s_%s_%dx%d_%d",
+                           itx_str[trh], itx_str[trv], width, height, bit_depth)) {
+                call_ref(ref_dst, ref_src, width, log2_transform_range);
+                call_new(new_dst, new_src, width, log2_transform_range);
+                checkasm_check_int32_t("vvc_itx_1d.asm", 0,
+                                       ref_dst, width * sizeof(*ref_dst),
+                                       new_dst, width * sizeof(*new_dst),
+                                       width, height, "dst");
+            }
+            bench_new(new_dst, new_src, width, 15);
         }
-        bench_new(new_dst, stride, new_src, stride);
     }
 }
 
 void checkasm_check_vvc_itx(void)
 {
     VVCDSPContext h;
-    ff_vvc_dsp_init(&h, 8, 0);
-    check_idct2(h, 8);
+    ff_vvc_dsp_init(&h, 8);
+    check_itx(h, DCT2, DCT2, 8);
+    ff_vvc_dsp_init(&h, 10);
+    check_itx(h, DCT2, DCT2, 10);
     report("idct2");
 }
