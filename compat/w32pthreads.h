4ba9308bb381bfffe51aa42a99cea42caf7b6869
Nuo Mi <nuomi2021@gmail.com>
2023-08-26 10:17:59 +0800

    Revert "configure: Set WIN32_LEAN_AND_MEAN at configure time"
    This reverts commit ddc1cd5cdd2570bf3d6ab807ee0ecfacdf09431d.

--- compat/w32pthreads.h
+++ compat/w32pthreads.h
@@ -35,6 +35,7 @@
  * As most functions here are used without checking return values,
  * only implement return values as necessary. */
 
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <process.h>
 #include <time.h>
--- libavutil/wchar_filename.h
+++ libavutil/wchar_filename.h
@@ -21,6 +21,7 @@
 
 #ifdef _WIN32
 
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include "mem.h"
 
