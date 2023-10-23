752c78fcf25736b7e1cb0d906e61a22aa94c67f6
Nuo Mi <nuomi2021@gmail.com>
2023-10-23 19:23:18 +0800

    refact: remove config_components.h

--- libavcodec/vvc/vvcdec.c
+++ libavcodec/vvc/vvcdec.c
@@ -20,8 +20,6 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-#include "config_components.h"
-
 #include "libavcodec/codec_internal.h"
 #include "libavcodec/decode.h"
 #include "libavcodec/profiles.h"
