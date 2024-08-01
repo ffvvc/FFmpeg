a0d9e50fc5cef198fe96011a53a728203a89652b
Wu Jianhua <toqsxw@outlook.com>
2024-08-02 04:25:34 +0800

    avcodec/cbs_sei_syntax_template: add sei message film_grain_characteristics
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- libavcodec/cbs_h2645.c
+++ libavcodec/cbs_h2645.c
@@ -2187,6 +2187,12 @@ static const SEIMessageTypeDescriptor cbs_sei_common_types[] = {
         sizeof(SEIRawAmbientViewingEnvironment),
         SEI_MESSAGE_RW(sei, ambient_viewing_environment),
     },
+    {
+        SEI_TYPE_FILM_GRAIN_CHARACTERISTICS,
+        1, 0,
+        sizeof(SEIRawFilmGrainCharacteristics),
+        SEI_MESSAGE_RW(sei, film_grain_characteristics),
+    },
     SEI_MESSAGE_TYPE_END,
 };
 
