78b47cced3f504c4cf012f2a11a5882f515b2237
Wu Jianhua <toqsxw@outlook.com>
2025-04-15 04:46:12 +0800

    avcodec/h26x/h2656_inter_template: fix put uni luma w functions for high bitdepth
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- libavcodec/h26x/h2656_inter_template.c
+++ libavcodec/h26x/h2656_inter_template.c
@@ -245,17 +245,13 @@ static void FUNC(put_uni_luma_w_h)(uint8_t *_dst,  const ptrdiff_t _dst_stride,
     const ptrdiff_t src_stride  = _src_stride / sizeof(pixel);
     const ptrdiff_t dst_stride  = _dst_stride / sizeof(pixel);
     const int8_t *filter        = hf;
-    const int ox                = _ox * (1 << (BIT_DEPTH - 8));
-    const int shift             = denom + 14 - BIT_DEPTH;
-#if BIT_DEPTH < 14
+    const int ox                = _ox * (1 << FFMIN(4, BIT_DEPTH - 8));
+    const int shift             = denom + FFMAX(2, 14 - BIT_DEPTH);
     const int offset            = 1 << (shift - 1);
-#else
-    const int offset            = 0;
-#endif
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++)
-            dst[x] = av_clip_pixel((((LUMA_FILTER(src, 1) >> (BIT_DEPTH - 8)) * wx + offset) >> shift) + ox);
+            dst[x] = av_clip_pixel((((LUMA_FILTER(src, 1) >> FFMIN(4, BIT_DEPTH - 8)) * wx + offset) >> shift) + ox);
         src += src_stride;
         dst += dst_stride;
     }
@@ -271,17 +267,13 @@ static void FUNC(put_uni_luma_w_v)(uint8_t *_dst,  const ptrdiff_t _dst_stride,
     const ptrdiff_t src_stride  = _src_stride / sizeof(pixel);
     const ptrdiff_t dst_stride  = _dst_stride / sizeof(pixel);
     const int8_t *filter        = vf;
-    const int ox                = _ox * (1 << (BIT_DEPTH - 8));
-    const int shift             = denom + 14 - BIT_DEPTH;
-#if BIT_DEPTH < 14
+    const int ox                = _ox * (1 << FFMIN(4, BIT_DEPTH - 8));
+    const int shift             = denom + FFMAX(2, 14 - BIT_DEPTH);
     const int offset            = 1 << (shift - 1);
-#else
-    const int offset            = 0;
-#endif
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++)
-            dst[x] = av_clip_pixel((((LUMA_FILTER(src, src_stride) >> (BIT_DEPTH - 8)) * wx + offset) >> shift) + ox);
+            dst[x] = av_clip_pixel((((LUMA_FILTER(src, src_stride) >> FFMIN(4, BIT_DEPTH - 8)) * wx + offset) >> shift) + ox);
         src += src_stride;
         dst += dst_stride;
     }
@@ -298,18 +290,14 @@ static void FUNC(put_uni_luma_w_hv)(uint8_t *_dst,  const ptrdiff_t _dst_stride,
     const ptrdiff_t src_stride  = _src_stride / sizeof(pixel);
     const ptrdiff_t dst_stride  = _dst_stride / sizeof(pixel);
     const int8_t *filter        = hf;
-    const int ox                = _ox * (1 << (BIT_DEPTH - 8));
-    const int shift             = denom + 14 - BIT_DEPTH;
-#if BIT_DEPTH < 14
+    const int ox                = _ox * (1 << FFMIN(4, BIT_DEPTH - 8));
+    const int shift             = denom + FFMAX(2, 14 - BIT_DEPTH);
     const int offset            = 1 << (shift - 1);
-#else
-    const int offset            = 0;
-#endif
 
     src   -= LUMA_EXTRA_BEFORE * src_stride;
     for (int y = 0; y < height + LUMA_EXTRA; y++) {
         for (int x = 0; x < width; x++)
-            tmp[x] = LUMA_FILTER(src, 1) >> (BIT_DEPTH - 8);
+            tmp[x] = LUMA_FILTER(src, 1) >> FFMIN(4, BIT_DEPTH - 8);
         src += src_stride;
         tmp += MAX_PB_SIZE;
     }
