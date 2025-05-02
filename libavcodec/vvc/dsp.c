376d1fcede6ab07e4a61ba73445375c509cd3628
Wu Jianhua <toqsxw@outlook.com>
2025-05-02 20:28:47 +0800

    avcodec/vvc/dsp: init dsp template for 16 bits
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- libavcodec/vvc/dsp.c
+++ libavcodec/vvc/dsp.c
@@ -87,6 +87,10 @@ typedef struct IntraEdgeParams {
 #include "dsp_template.c"
 #undef BIT_DEPTH
 
+#define BIT_DEPTH 16
+#include "dsp_template.c"
+#undef BIT_DEPTH
+
 void ff_vvc_dsp_init(VVCDSPContext *vvcdsp, int bit_depth)
 {
 #undef FUNC
@@ -102,6 +106,9 @@ void ff_vvc_dsp_init(VVCDSPContext *vvcdsp, int bit_depth)
     FUNC(ff_vvc_alf_dsp_init, depth)(&vvcdsp->alf);                             \
 
     switch (bit_depth) {
+    case 16:
+        VVC_DSP(16);
+        break;
     case 14:
         VVC_DSP(14);
         break;
